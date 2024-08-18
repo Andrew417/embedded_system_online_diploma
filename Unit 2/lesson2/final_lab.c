81113a5814b90d5ea8fc6eb543105b050ce8e56d
andrew ehab <andrewehab417@gmai.com>
2024-08-18 23:53:01 +0300

    final lab solved correctly

---
+++ Unit 2/lesson2/final_lab.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+    int i=0,j=0;
+    for(j;j<10;j++){
+        for(i=j;i<10;i++){
+            printf("%d ",i);
+        }
+        printf("\r\n");
+    }
+
+
+
+}
